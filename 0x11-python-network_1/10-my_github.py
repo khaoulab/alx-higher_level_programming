6b130869bbc22819c972a4a05bce899521349b8e
khaoulab <khaoula.belmqeddem@gmail.com>
2024-01-28 08:37:10 -0800

    0x11-python-network_1

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+takes your Github credentials (username and password) and uses the
+Github API to display your id
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    headers = {'Authorization': f'Bearer {password}'}
+    res = requests.get("https://api.github.com/user", headers=headers)
+    if (res.status_code >= 400):
+        print("None")
+        exit()
+    print(res.json()["id"])
---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+takes in a URL and an email, sends a POST request to the passed URL
+with the email as a parameter, and displays the body of the
+response (decoded in utf-8)
+"""
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    try:
+        url = sys.argv[1]
+        with urllib.request.urlopen(url) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
